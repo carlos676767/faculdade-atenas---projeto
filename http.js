7fe6d86a769d90b980919fdb5422330f49774955
João V. Gaspar <jhone_ag@hotmail.com>
2024-12-02 19:46:48 -0300

    Criação do formulario com o modal e conexão com o bd

--- http.js
+++ http.js
@@ -11,4 +11,11 @@ async function httpRequest(nome, email) {
   console.log(response);
 }
 
-httpRequest();
+function registrarUsuario() {
+
+  const nome = document.getElementById('InputName1').value;
+  const email = document.getElementById('InputEmail1').value;
+  const telefone = document.getElementById('InputNumber1').value;
+
+  httpRequest(nome, email, telefone);
+}
