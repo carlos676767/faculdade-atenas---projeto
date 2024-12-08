876bead2c8f8a07891386d680f7b1b824e814b4e
Kaik Neres <kaikneres123@gmail.com>
2024-12-08 14:51:57 -0300

    alteração

--- http.js
+++ http.js
@@ -1,6 +1,7 @@
 
 
 class Registro {
+  static buttom = document .querySelector('button')
   static async httpRequest(nome, email){
     const httpRequest = await fetch(`http://localhost:8080/cadastrar`, {
       method: `POST`,
@@ -19,5 +20,11 @@ class Registro {
     const nome = document.getElementById('InputName1').value;
     const email = document.getElementById('InputEmail1').value;
     await this.httpRequest(nome, email)
+  }  
+  static buttomevent () {
+  this.buttom.addEventListener('click',this.httpRequest() )
+
   }
-}
+}  
+
+  Registro.buttomevent()
